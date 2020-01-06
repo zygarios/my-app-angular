1b5dfd3c0a1792606e7ba1175ac2742273d16575
Bartosz Zygmunt <bartekzygmunt92@gmail.com>
2020-01-06 16:24:33 +0100

    add basic features

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Recipe } from "../recipe.model";
 
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  selector: "app-recipe-list",
+  templateUrl: "./recipe-list.component.html",
+  styleUrls: ["./recipe-list.component.css"]
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      "A Test Recipe",
+      "This is simply a test",
+      "https://cdn.pixabay.com/photo/2018/07/29/22/39/bread-3571266_960_720.jpg"
+    ),
+    new Recipe(
+      "Second Test Recipe",
+      "This is second simply a test",
+      "https://cdn.pixabay.com/photo/2018/05/30/19/18/burger-3442227_960_720.jpg"
+    )
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
